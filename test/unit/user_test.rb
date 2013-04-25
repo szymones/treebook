3273fee73df714898dcb18e5ffefdb1b6d9f1d56
Szymon Pawlik <szymon.pawlik@gmail.com>
2013-04-25 09:45:47 +0200

    Fixed bug with profile_name validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -31,15 +31,20 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test " prfile name should be provided without spaces" do 
+    user = User.new
+    user.profile_name = "My profile with spaces"
 
-  	user = User.new
-  	user.profile_name = "My profile with spaces"
-
-  	assert !user.save
-  	assert !user.errors[:profile_name].empty?
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
   	assert !user.errors[:profile_name].include?("Nie moze byc ze spacjami")
+  end
 
+  test " a user should be able to register with corrct profile name" do
+    user = User.new(first_name: "Bartek", last_name:'Pawlik',email:"bp@test.pl")
+    user.password = user.password_confirmation = "asdfasdf"
 
+    user.profile_name = "koko1"
+    assert user.valid?
   end
 
 end
